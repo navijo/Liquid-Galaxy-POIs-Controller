54e7f1af8bafaefb7e85cbf8f1cf19a5185be330
Ivan Josa <navijo@gmail.com>
2016-07-05 09:45:21 +0200

    Added the possibility to upload the script file to a task via file

--- app/src/main/java/com/example/rafa/liquidgalaxypoiscontroller/advancedTools/CreateTaskFragment.java
+++ app/src/main/java/com/example/rafa/liquidgalaxypoiscontroller/advancedTools/CreateTaskFragment.java
@@ -10,6 +10,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
@@ -28,12 +29,23 @@ import android.widget.Toast;
 import com.example.rafa.liquidgalaxypoiscontroller.R;
 import com.example.rafa.liquidgalaxypoiscontroller.data.POIsContract;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
 
 /**
  * Created by lgwork on 25/05/16.
  */
 public class CreateTaskFragment extends DialogFragment {
+
+    private static int ACTION_UPLOAD_LOGO = 1;
+    private static int ACTION_UPLOAD_EXEC_SCRIPT = 2;
+    private static int ACTION_UPLOAD_SHUTDOWN_SCRIPT = 3;
+
 
     Handler handler;
     private EditText new_task_name_input;
@@ -47,6 +59,9 @@ public class CreateTaskFragment extends DialogFragment {
     private EditText new_task_browser_URL;
     private Button pickPhotoBtn;
     private ImageView iconview;
+
+    private Button uploadExecutionScript;
+    private Button uploadShutdownScript;
 
     public static CreateTaskFragment newInstance() {
         CreateTaskFragment createTask = new CreateTaskFragment();
@@ -90,20 +105,58 @@ public class CreateTaskFragment extends DialogFragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (data != null) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+        if (requestCode == ACTION_UPLOAD_LOGO) {
+            if (data != null) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
 
-            // Get the cursor
-            Cursor cursor = getActivity().getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
-            // Move to first row
-            cursor.moveToFirst();
+                // Get the cursor
+                Cursor cursor = getActivity().getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
 
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String imgPath = cursor.getString(columnIndex);
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String imgPath = cursor.getString(columnIndex);
 
-            iconview.setImageBitmap(BitmapFactory.decodeFile(imgPath));
+                iconview.setImageBitmap(BitmapFactory.decodeFile(imgPath));
+            }
+        } else if (requestCode == ACTION_UPLOAD_EXEC_SCRIPT) {
+            if (data != null) {
+
+                Uri uri = data.getData();
+                if (uri != null) {
+                    String path = uri.toString();
+                    String filePath;
+                    //We get the file path by executing one of the following methods, depending the explorer the user uses.
+                    if (path.toLowerCase().startsWith("file://")) {
+                        filePath = (new File(URI.create(path))).getAbsolutePath();
+                    } else {
+                        filePath = pathTreatment(data.getData().getPath(), Environment.getExternalStorageDirectory().getAbsolutePath());
+                    }
+                    String readedFile = readFile(filePath);
+                    new_task_script_input.setText(readedFile);
+                }
+
+            }
+
+        } else if (requestCode == ACTION_UPLOAD_SHUTDOWN_SCRIPT) {
+            if (data != null) {
+
+                Uri uri = data.getData();
+                if (uri != null) {
+                    String path = uri.toString();
+                    String filePath;
+                    //We get the file path by executing one of the following methods, depending the explorer the user uses.
+                    if (path.toLowerCase().startsWith("file://")) {
+                        filePath = (new File(URI.create(path))).getAbsolutePath();
+                    } else {
+                        filePath = pathTreatment(data.getData().getPath(), Environment.getExternalStorageDirectory().getAbsolutePath());
+                    }
+                    String readedFile = readFile(filePath);
+                    new_task_shutdown_script_input.setText(readedFile);
+                }
+            }
         }
     }
 
@@ -122,7 +175,7 @@ public class CreateTaskFragment extends DialogFragment {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(i, 0);
+                startActivityForResult(i, ACTION_UPLOAD_LOGO);
             }
         });
 
@@ -138,6 +191,31 @@ public class CreateTaskFragment extends DialogFragment {
 
         new_task_description_input = (EditText) rootView.findViewById(R.id.new_task_description_input);
         new_task_script_input = (EditText) rootView.findViewById(R.id.new_task_script_input);
+
+        uploadExecutionScript = (Button) rootView.findViewById(R.id.uploadExecutionScript);
+        uploadExecutionScript.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                startActivityForResult(intent, ACTION_UPLOAD_EXEC_SCRIPT);
+            }
+        });
+
+        uploadShutdownScript = (Button) rootView.findViewById(R.id.uploadShutDownScript);
+        uploadShutdownScript.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                startActivityForResult(intent, ACTION_UPLOAD_SHUTDOWN_SCRIPT);
+            }
+        });
+
 
         saveTask.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -189,6 +267,63 @@ public class CreateTaskFragment extends DialogFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+    }
+
+    private String pathTreatment(String path, String absolutePath) {
+        int start = 0;
+        String firstPathFolder = "";
+        if (path.contains(":")) {
+            start = path.indexOf(":") + 1;
+            path = path.substring(start);
+        }
+
+        if (path.startsWith("/")) {
+            firstPathFolder = path.split("/")[1];
+        } else {
+            firstPathFolder = path.split("/")[0];
+        }
+        String[] absoluteFolders = absolutePath.split("/");
+        String lastAbsoluteFolder = absoluteFolders[absoluteFolders.length - 1];
+
+        if (firstPathFolder.equals(lastAbsoluteFolder)) {
+            if (path.startsWith("/")) {
+                path = path.substring(firstPathFolder.length() + 2);
+            } else {
+                path = path.substring(firstPathFolder.length() + 1);
+            }
+        }
+
+        if (!absolutePath.endsWith("/")) {
+            absolutePath = absolutePath + "/";
+        }
+        return absolutePath + path;
+    }
+
+    private String readFile(String filePath) {
+        StringBuilder readedStringBuilder = new StringBuilder();
+        File file = new File(filePath);
+        if (file.exists()) {
+
+            try {
+                FileInputStream inputStream = null;
+                inputStream = new FileInputStream(file);
+                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                String line = "";
+
+                while ((line = br.readLine()) != null) {
+                    if (!line.equals("") && line != null) {
+                        readedStringBuilder.append(line);
+                        readedStringBuilder.append("\n");
+                    }
+                }
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(getActivity(), "File couldn't be opened. Try to open it with a different file explorer, for example 'Root Explorer' once.", Toast.LENGTH_LONG).show();
+        }
+        return readedStringBuilder.toString();
     }
 
 }
--- app/src/main/java/com/example/rafa/liquidgalaxypoiscontroller/advancedTools/EditTaskFragment.java
+++ app/src/main/java/com/example/rafa/liquidgalaxypoiscontroller/advancedTools/EditTaskFragment.java
@@ -10,6 +10,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
@@ -28,12 +29,22 @@ import android.widget.Toast;
 import com.example.rafa.liquidgalaxypoiscontroller.R;
 import com.example.rafa.liquidgalaxypoiscontroller.data.POIsContract;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
 
 /**
  * Created by lgwork on 25/05/16.
  */
 public class EditTaskFragment extends DialogFragment {
+
+    private static int ACTION_UPLOAD_LOGO = 1;
+    private static int ACTION_UPLOAD_EXEC_SCRIPT = 2;
+    private static int ACTION_UPLOAD_SHUTDOWN_SCRIPT = 3;
 
     Handler handler;
     private EditText edit_task_name_input;
@@ -49,6 +60,10 @@ public class EditTaskFragment extends DialogFragment {
     private Button deletePhotoBtn;
     private ImageView iconview;
     private long taskId;
+
+    private Button uploadExecutionScript;
+    private Button uploadShutdownScript;
+
 
     public static EditTaskFragment newInstance(long taskId) {
         EditTaskFragment createTask = new EditTaskFragment();
@@ -126,7 +141,7 @@ public class EditTaskFragment extends DialogFragment {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(i, 0);
+                startActivityForResult(i, ACTION_UPLOAD_LOGO);
             }
         });
 
@@ -155,6 +170,30 @@ public class EditTaskFragment extends DialogFragment {
 
         edit_task_description_input.setText(currentTask.getDescription());
         edit_task_script_input.setText(currentTask.getScript());
+
+        uploadExecutionScript = (Button) rootView.findViewById(R.id.uploadExecutionScript);
+        uploadExecutionScript.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                startActivityForResult(intent, ACTION_UPLOAD_EXEC_SCRIPT);
+            }
+        });
+
+        uploadShutdownScript = (Button) rootView.findViewById(R.id.uploadShutDownScript);
+        uploadShutdownScript.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                startActivityForResult(intent, ACTION_UPLOAD_SHUTDOWN_SCRIPT);
+            }
+        });
 
         saveTask.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -203,20 +242,57 @@ public class EditTaskFragment extends DialogFragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (data != null) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+        if (requestCode == ACTION_UPLOAD_LOGO) {
+            if (data != null) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
 
-            // Get the cursor
-            Cursor cursor = getActivity().getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
-            // Move to first row
-            cursor.moveToFirst();
+                // Get the cursor
+                Cursor cursor = getActivity().getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
 
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String imgPath = cursor.getString(columnIndex);
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String imgPath = cursor.getString(columnIndex);
 
-            iconview.setImageBitmap(BitmapFactory.decodeFile(imgPath));
+                iconview.setImageBitmap(BitmapFactory.decodeFile(imgPath));
+            }
+        } else if (requestCode == ACTION_UPLOAD_EXEC_SCRIPT) {
+            if (data != null) {
+
+                Uri uri = data.getData();
+                if (uri != null) {
+                    String path = uri.toString();
+                    String filePath;
+                    //We get the file path by executing one of the following methods, depending the explorer the user uses.
+                    if (path.toLowerCase().startsWith("file://")) {
+                        filePath = (new File(URI.create(path))).getAbsolutePath();
+                    } else {
+                        filePath = pathTreatment(data.getData().getPath(), Environment.getExternalStorageDirectory().getAbsolutePath());
+                    }
+                    String readedFile = readFile(filePath);
+                    edit_task_script_input.setText(readedFile);
+                }
+
+            }
+        } else if (requestCode == ACTION_UPLOAD_SHUTDOWN_SCRIPT) {
+            if (data != null) {
+
+                Uri uri = data.getData();
+                if (uri != null) {
+                    String path = uri.toString();
+                    String filePath;
+                    //We get the file path by executing one of the following methods, depending the explorer the user uses.
+                    if (path.toLowerCase().startsWith("file://")) {
+                        filePath = (new File(URI.create(path))).getAbsolutePath();
+                    } else {
+                        filePath = pathTreatment(data.getData().getPath(), Environment.getExternalStorageDirectory().getAbsolutePath());
+                    }
+                    String readedFile = readFile(filePath);
+                    edit_task_shutdown_script_input.setText(readedFile);
+                }
+            }
         }
     }
 
@@ -247,6 +323,64 @@ public class EditTaskFragment extends DialogFragment {
         }
 
         return lgTask;
+    }
+
+
+    private String pathTreatment(String path, String absolutePath) {
+        int start = 0;
+        String firstPathFolder = "";
+        if (path.contains(":")) {
+            start = path.indexOf(":") + 1;
+            path = path.substring(start);
+        }
+
+        if (path.startsWith("/")) {
+            firstPathFolder = path.split("/")[1];
+        } else {
+            firstPathFolder = path.split("/")[0];
+        }
+        String[] absoluteFolders = absolutePath.split("/");
+        String lastAbsoluteFolder = absoluteFolders[absoluteFolders.length - 1];
+
+        if (firstPathFolder.equals(lastAbsoluteFolder)) {
+            if (path.startsWith("/")) {
+                path = path.substring(firstPathFolder.length() + 2);
+            } else {
+                path = path.substring(firstPathFolder.length() + 1);
+            }
+        }
+
+        if (!absolutePath.endsWith("/")) {
+            absolutePath = absolutePath + "/";
+        }
+        return absolutePath + path;
+    }
+
+    private String readFile(String filePath) {
+        StringBuilder readedStringBuilder = new StringBuilder();
+        File file = new File(filePath);
+        if (file.exists()) {
+
+            try {
+                FileInputStream inputStream = null;
+                inputStream = new FileInputStream(file);
+                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                String line = "";
+
+                while ((line = br.readLine()) != null) {
+                    if (!line.equals("") && line != null) {
+                        readedStringBuilder.append(line);
+                        readedStringBuilder.append("\n");
+                    }
+                }
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(getActivity(), "File couldn't be opened. Try to open it with a different file explorer, for example 'Root Explorer' once.", Toast.LENGTH_LONG).show();
+        }
+        return readedStringBuilder.toString();
     }
 
 }
