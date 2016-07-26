c1839a207f377503d9093a0dc92a2dd84b11fe97
Ivan Josa <navijo@gmail.com>
2016-07-26 18:15:50 +0200

    Rotation and visualization of POIS

--- app/src/main/java/com/example/rafa/liquidgalaxypoiscontroller/utils/PoisGridViewAdapter.java
+++ app/src/main/java/com/example/rafa/liquidgalaxypoiscontroller/utils/PoisGridViewAdapter.java
@@ -4,13 +4,15 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.rafa.liquidgalaxypoiscontroller.R;
@@ -29,11 +31,14 @@ public class PoisGridViewAdapter extends BaseAdapter {
     Context context;
     Activity activity;
     Session session;
+    private LayoutInflater mInflater;
 
     public PoisGridViewAdapter(List<POI> poiList, Context context, Activity activity) {
         this.poiList = poiList;
         this.context = context;
         this.activity = activity;
+
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         GetSessionTask getSessionTask = new GetSessionTask();
         getSessionTask.execute();
@@ -59,28 +64,103 @@ public class PoisGridViewAdapter extends BaseAdapter {
     public View getView(int i, View view, ViewGroup viewGroup) {
         final POI currentPoi = this.poiList.get(i);
 
-        Button button = new Button(context);
-        String displayName = currentPoi.getName();
-        button.setText(displayName);
 
-        Drawable top = context.getResources().getDrawable(R.drawable.ic_place_black_24dp);
-        button.setCompoundDrawablesWithIntrinsicBounds(top, null, null, null);
+        AbsListView.LayoutParams params = new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT, AbsListView.LayoutParams.MATCH_PARENT);
 
-        AbsListView.LayoutParams params = new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT, AbsListView.LayoutParams.WRAP_CONTENT);
-        button.setMaxLines(1);
+        RelativeLayout layout = new RelativeLayout(context);
+        layout.setBackground(context.getResources().getDrawable(R.drawable.button_rounded_grey));
+        layout.setLayoutParams(params);
 
-        button.setBackground(context.getResources().getDrawable(R.drawable.button_rounded_grey));
-        button.setLayoutParams(params);
-        button.setOnClickListener(new View.OnClickListener() {
+        RelativeLayout.LayoutParams paramsView = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        ImageButton viewPoiButton = new ImageButton(context);
+        paramsView.addRule(RelativeLayout.CENTER_VERTICAL);
+        paramsView.addRule(RelativeLayout.ALIGN_START);
+        viewPoiButton.setImageDrawable(context.getResources().getDrawable(R.drawable.ic_place_black_24dp));
+        viewPoiButton.setBackground(context.getResources().getDrawable(R.drawable.button_rounded_grey));
+        viewPoiButton.setLayoutParams(paramsView);
+        viewPoiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String command = buildCommand(currentPoi);
-                VisitPoiTask visitPoiTask = new VisitPoiTask(command, currentPoi);
+                VisitPoiTask visitPoiTask = new VisitPoiTask(command, currentPoi, false);
                 visitPoiTask.execute();
             }
         });
 
-        return button;
+        layout.addView(viewPoiButton);
+
+
+        // RelativeLayout.LayoutParams paramsText = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        RelativeLayout.LayoutParams paramsText = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        TextView poiName = new TextView(context);
+        if (currentPoi.getName().length() > 20) {
+            String name = currentPoi.getName().substring(0, 20) + "...";
+            poiName.setText(name);
+        } else {
+            poiName.setText(currentPoi.getName());
+        }
+
+        poiName.setTextSize(20);
+        poiName.setMaxLines(2);
+        paramsText.addRule(RelativeLayout.CENTER_IN_PARENT);
+
+
+        poiName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String command = buildCommand(currentPoi);
+                VisitPoiTask visitPoiTask = new VisitPoiTask(command, currentPoi, false);
+                visitPoiTask.execute();
+            }
+        });
+        layout.addView(poiName);
+        poiName.setLayoutParams(paramsText);
+
+
+        RelativeLayout.LayoutParams paramsRotate = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        ImageButton rotatePoiButton = new ImageButton(context);
+        rotatePoiButton.setImageDrawable(context.getResources().getDrawable(R.drawable.rotate_36));
+        rotatePoiButton.setBackground(context.getResources().getDrawable(R.drawable.button_rounded_grey));
+        paramsRotate.addRule(RelativeLayout.ALIGN_PARENT_END);
+
+
+        rotatePoiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String command = buildCommand(currentPoi);
+                VisitPoiTask visitPoiTask = new VisitPoiTask(command, currentPoi, true);
+                visitPoiTask.execute();
+            }
+        });
+
+        layout.addView(rotatePoiButton);
+        rotatePoiButton.setLayoutParams(paramsRotate);
+
+//
+//        Button button = new Button(context);
+//        String displayName = currentPoi.getName();
+//        button.setText(displayName);
+//
+//        Drawable top = context.getResources().getDrawable(R.drawable.ic_place_black_24dp);
+//        button.setCompoundDrawablesWithIntrinsicBounds(top, null, null, null);
+//
+//        AbsListView.LayoutParams params = new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT, AbsListView.LayoutParams.WRAP_CONTENT);
+//        button.setMaxLines(1);
+//
+//        button.setBackground(context.getResources().getDrawable(R.drawable.button_rounded_grey));
+//        button.setLayoutParams(params);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String command = buildCommand(currentPoi);
+//                VisitPoiTask visitPoiTask = new VisitPoiTask(command, currentPoi);
+//                visitPoiTask.execute();
+//            }
+//        });
+//
+//        return button;
+
+        return layout;
     }
 
 
@@ -100,11 +180,13 @@ public class PoisGridViewAdapter extends BaseAdapter {
 
         String command;
         POI currentPoi;
+        boolean rotate;
         private ProgressDialog dialog;
 
-        public VisitPoiTask(String command, POI currentPoi) {
+        public VisitPoiTask(String command, POI currentPoi, boolean rotate) {
             this.command = command;
             this.currentPoi = currentPoi;
+            this.rotate = rotate;
         }
 
         @Override
@@ -140,39 +222,41 @@ public class PoisGridViewAdapter extends BaseAdapter {
             try {
                 LGUtils.setConnectionWithLiquidGalaxy(session, command, activity);
 
-                Thread.sleep(14000);
+                if (this.rotate) {
+                    Thread.sleep(14000);
 
-                while (!isCancelled()) {
-                    session.sendKeepAliveMsg();
-
-                    for (int i = 0; i < 180; i = i + 90) {
-                        String commandRotate = "echo 'flytoview=<gx:duration>6</gx:duration><gx:flyToMode>smooth</gx:flyToMode><LookAt><longitude>" + this.currentPoi.getLongitude() +
-                                "</longitude><latitude>" + this.currentPoi.getLatitude() +
-                                "</latitude><altitude>" + this.currentPoi.getAltitude() +
-                                "</altitude><heading>" + (this.currentPoi.getHeading() + i) +
-                                "</heading><tilt>" + this.currentPoi.getTilt() +
-                                "</tilt><range>" + this.currentPoi.getRange() +
-                                "</range><gx:altitudeMode>" + this.currentPoi.getAltitudeMode() +
-                                "</gx:altitudeMode></LookAt>' > /tmp/query.txt";
-
-                        LGUtils.setConnectionWithLiquidGalaxy(session, commandRotate, activity);
+                    while (!isCancelled()) {
                         session.sendKeepAliveMsg();
-                        Thread.sleep(6000);
-                    }
 
-                    for (int i = -180; i <= 0; i = i + 90) {
-                        String commandRotate = "echo 'flytoview=<gx:duration>6</gx:duration><gx:flyToMode>smooth</gx:flyToMode><LookAt><longitude>" + this.currentPoi.getLongitude() +
-                                "</longitude><latitude>" + this.currentPoi.getLatitude() +
-                                "</latitude><altitude>" + this.currentPoi.getAltitude() +
-                                "</altitude><heading>" + (this.currentPoi.getHeading() + i) +
-                                "</heading><tilt>" + this.currentPoi.getTilt() +
-                                "</tilt><range>" + this.currentPoi.getRange() +
-                                "</range><gx:altitudeMode>" + this.currentPoi.getAltitudeMode() +
-                                "</gx:altitudeMode></LookAt>' > /tmp/query.txt";
+                        for (int i = 0; i < 180; i = i + 90) {
+                            String commandRotate = "echo 'flytoview=<gx:duration>6</gx:duration><gx:flyToMode>smooth</gx:flyToMode><LookAt><longitude>" + this.currentPoi.getLongitude() +
+                                    "</longitude><latitude>" + this.currentPoi.getLatitude() +
+                                    "</latitude><altitude>" + this.currentPoi.getAltitude() +
+                                    "</altitude><heading>" + (this.currentPoi.getHeading() + i) +
+                                    "</heading><tilt>" + this.currentPoi.getTilt() +
+                                    "</tilt><range>" + this.currentPoi.getRange() +
+                                    "</range><gx:altitudeMode>" + this.currentPoi.getAltitudeMode() +
+                                    "</gx:altitudeMode></LookAt>' > /tmp/query.txt";
 
-                        LGUtils.setConnectionWithLiquidGalaxy(session, commandRotate, activity);
-                        session.sendKeepAliveMsg();
-                        Thread.sleep(6000);
+                            LGUtils.setConnectionWithLiquidGalaxy(session, commandRotate, activity);
+                            session.sendKeepAliveMsg();
+                            Thread.sleep(6000);
+                        }
+
+                        for (int i = -180; i <= 0; i = i + 90) {
+                            String commandRotate = "echo 'flytoview=<gx:duration>6</gx:duration><gx:flyToMode>smooth</gx:flyToMode><LookAt><longitude>" + this.currentPoi.getLongitude() +
+                                    "</longitude><latitude>" + this.currentPoi.getLatitude() +
+                                    "</latitude><altitude>" + this.currentPoi.getAltitude() +
+                                    "</altitude><heading>" + (this.currentPoi.getHeading() + i) +
+                                    "</heading><tilt>" + this.currentPoi.getTilt() +
+                                    "</tilt><range>" + this.currentPoi.getRange() +
+                                    "</range><gx:altitudeMode>" + this.currentPoi.getAltitudeMode() +
+                                    "</gx:altitudeMode></LookAt>' > /tmp/query.txt";
+
+                            LGUtils.setConnectionWithLiquidGalaxy(session, commandRotate, activity);
+                            session.sendKeepAliveMsg();
+                            Thread.sleep(6000);
+                        }
                     }
                 }
 
